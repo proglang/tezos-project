5abd6892684aa6309ee3198ae7c8a5d27b463a5d
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2020-07-10 15:37:41 +0200

    Remove unsuccessful approach

--- api/SyncAPIV0.mli
+++ api/SyncAPIV0.mli
@@ -10,13 +10,19 @@ type op_id_t
 (* network port number *)
 type port_nr = int
 
+type synchAPI
+
 (* set the port for communicating with the local Tezos node 
  * default = 8732
  *)
-val set_port : port_nr -> unit
+val set_port : ?p:port_nr -> unit -> synchAPI
+
+val get_port : synchAPI -> port_nr
 
 val get_puk_from_nickname : string -> puk
 
+(*val get_alias_file_contents: unit -> string*)
+  
 val get_puk_from_hash : string -> puk
 
 type failure_message = Insufficient_balance | Counter_mismatch | Invalid_receiver
