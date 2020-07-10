0b11daade53586cc783747ef6c1b55710a6ef67f
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2020-07-10 17:02:57 +0200

    Extract result from tzresult monad

--- sandbox/test.ml
+++ sandbox/test.ml
@@ -4,8 +4,13 @@ let run =
   Internal_event_unix.init ()
   >>= fun () ->
   Api.get_puk "tamara"
-  >>= fun _ ->
-  Internal_event_unix.close () >>= fun () -> Lwt.return 1 
+  >>= fun result ->
+  match result with
+  | Ok i -> print_endline @@ string_of_int i; Lwt.return 1
+  | _ -> print_endline "Error"; Lwt.return 0
+  >>= fun retcode ->
+  Internal_event_unix.close () >>= fun () -> Lwt.return retcode 
+
 let () =
   Lwt_exit.exit_on Sys.sigint ;
   Lwt_exit.exit_on Sys.sigterm ;
