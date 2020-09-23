8fd94578926000b27735da81ca7d2f3f3e19add7
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2020-09-23 11:29:29 +0200

    Update documentation

--- sandbox/lib/api.mli
+++ sandbox/lib/api.mli
@@ -1,10 +1,10 @@
 (** This API provides functions to interact with a Tezos node *)
 open Tezos_protocol_006_PsCARTHA.Protocol.Alpha_context
 
-(** A public key *)
+(** A public key of an account (implicit or originated)*)
 type puk = Signature.public_key
 
-(** A public key hash *)
+(** A public key hash of an account (implicit or originated) *)
 type pukh = Signature.public_key_hash
 
  (** Contract representation (implicit or originated) *)
@@ -12,7 +12,7 @@ type contract = Contract.t
 
 type 'a tz_result = 'a tzresult Lwt.t
 
-(** Amount of Tezos tokens (tez) *)
+(** Representation of Tezos tokens (tezzies) *)
 type tez = float
 
  (** Operation hash *)
@@ -31,54 +31,95 @@ type failure_message = Insufficient_balance
                      | Operation_quota_exceeded (* Needed? *)
                      | Storage_limit_too_high (* Needed? *)
                      | Cannot_pay_storage_fee (* Handled by Insufficient_fee? *)
-                     | Unknown
+                     | Unknown (** None of the above match - error list should be extended if this occurs *)
 
 (** Result of a transaction injection*)
 type answer = Pending of oph (** The operation hash of the successfully injected transaction *)
-            | Fail of failure_message (** An error with respective {!type:failure_message} *)
+            | Fail of failure_message (** An error with the respective {!type:failure_message} *)
 
-(** Result of an included operation *)
-type result = unit (* which information is returned??? *)
+(** Result of an operation which has been successfully included into a block *)
+type result = {
+    block_hash : blockh; (** Block in which transaction was included *)
+    rpc_position : (int * int); (** The indices where the operation can be found within the block *)
+    balance_updates : Delegate.balance_updates; (** List of balance updates *)
+    consumed_gas : int; (** Amount of gas burned during execution *)
+    storage : Script.expr option; (** Contents of storage *)
+    originated_contracts : contract list; (** List of originated contracts *)
+    storage_size : int; (** Amount of storage used during transaction *)
+    paid_storage_size_diff : int ; (** Storage fees paid *)
+    big_map_diff : Contract.big_map_diff option; (** Changes in the BigMap *)
+    allocated_destination_contract : bool} (** Indicates whether the dest account was empty or not *)
 
 (** Possible reasons for a transaction rejection *)
-type reason = Timeout (** The transaction timed out and was removed from the Mempool *)
+type reason = Timeout (** The transaction timed out and was removed from the Mempool (tbd if this case can be distinguished clearly from others) *)
             | Skipped (** The transaction was skipped due to a previously failed operation *)
             | Backtracked (** The transaction was backtracked due to a subsequently failed operation *)
-            | Reason of failure_message  (** The transaction failed due to any error defined by {!type: failure_message} *)
+            | Reason of failure_message  (** The transaction failed due to {!type: failure_message} *)
 
-(** Errors unrelated to a transaction *)
+(** Errors which might occur during retrieval of transaction status (unrelated to the transaction result) *)
 type error_message = RPC_error of {uri: string} (** Error occured during RPC call *)
-                   | Unexpected_result (** The {!type:oph} did not refer to a transaction operation*)
-                   | Unknown (** Generic error *)
+                   | Unexpected_result (** The {!type:oph} did not refer to a transaction operation *)
+                   | Unknown (** Unknown error - error list should be extended if this occurs *)
 
 (** Status of an injected transaction *)
-type status = Still_pending (** Transaction hasn't been included yet *)
-            | Accepted of result (** Transaction was included *)
-            | Rejected of reason (** Transaction was rejected due to any {!type:reason} *)
-            | Missing (** Transaction couldn't be found *)
+type status = Still_pending (** Transaction hasn't been included yet (prevalidated, delayed or unprocessed) *)
+            | Accepted of result (** Transaction was included with {!type:result} *)
+            | Rejected of reason (** Transaction was rejected due to {!type:reason} *)
+            | Missing (** Transaction couldn't be found (tbd should this be timeout?) *)
             | Error of error_message (** Status couldn't be retrieved due to {!type:error_message} *)
 
-(** [get_puk_from_alias s] expects an alias string of an implicit account and returns
+(** [get_puk_from_alias s] expects an alias of an implicit account and returns
     the associated public key of the account.
-    @return {!type:puk} *)
+    @param s alias of implicit account
+    @return {!type:puk} the associated public key
+*)
 val get_puk_from_alias: string -> puk tz_result
 
+(** [get_puk_from_hash s] expects a public key hash as string and returns the associated
+    public key of the account.
+    @param s public key hash
+    @return {!type:puk} the associated public key
+ *)
 val get_puk_from_hash: string -> puk tz_result
 
+(** [get_pukh_from_alias s] expects an alias of an implicit account and returns 
+    the associated public key hash.
+    @param s alias of implicit account
+    @return {!type:pukh} the associated public key hash
+*)
 val get_pukh_from_alias: string -> pukh tz_result
 
+(** [get_contract s] expects an alias, public key hash or contract hash as string
+    and returns the associated contract representation.
+    @param s alias, public key hash or contract hash
+    @return {!type:contract} the associated contract representation
+*)
 val get_contract: string -> contract tz_result
 
+(** [set_port p] specifies under which port the RPC interface of the node is
+    reachable if the default (8732) does not apply.
+    @param p the RPC port of the node
+*)
 val set_port: int -> unit
 
+(** [set_basedir d] specifies the path of the tezos-client base directory
+    (normally /home/<username>/.tezos-client/)
+    @param d path of the tezos-client base directory
+*)
 val set_basedir: string -> unit
 
+(** [transfer a src dst fee] injects a transfer transaction.
+    @param a the amount of tezzies to be transferred
+    @param src the public key hash of the sender
+    @param dst the contract representation of the receiver
+    @param fee the amount of fees to be paid for the baker
+    @return {!type:answer} the operation hash of the injected transaction or
+    error
+*)
 val transfer: tez -> pukh -> contract -> tez -> answer Lwt.t
 
-val exception_handler : exn -> 'a tzresult Lwt.t
-
 (** [query op] retrieves the current status of an injected transaction
-    @param oph the operation hash of the injected transaction
-    @return {!type:status}
+    @param op the operation hash of the injected transaction
+    @return {!type:status} a status or error
 *)
 val query : oph -> status Lwt.t
