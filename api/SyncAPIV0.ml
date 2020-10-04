4c81058abe6aaca07c70a7a37328c117006a4f82
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2020-10-04 18:13:08 +0200

    Updated current API state

--- api/SyncAPIV0.ml
+++ api/SyncAPIV0.ml
@@ -1,5 +1,5 @@
 open Client_keys
-open Tezos_client_006_PsCARTHA.Client_proto_context
+open Tezos_client_006_PsCARTHA
 open Tezos_client_006_PsCARTHA.Protocol_client_context
 open Tezos_client_006_PsCARTHA.Injection
 open Tezos_client_006_PsCARTHA.Client_proto_contracts
@@ -7,10 +7,10 @@ open Tezos_protocol_006_PsCARTHA.Protocol.Alpha_context
 open Tezos_raw_protocol_006_PsCARTHA
 open Tezos_protocol_environment_006_PsCARTHA
 open Apply_results
+open SyncAPIV0_error
 open SyncAPIV0_context
 open Format
 open Base
-open Str
 open Int64
 
 type puk = Signature.public_key
@@ -22,27 +22,24 @@ type blockh = Block_hash.t
 module Tez_t : sig
   type t = Tez.t
   val tez : float -> t
+  val zero : t
+  val to_float : Tez.t -> float
 end = struct
   type t = Tez.t
+  let conversion_factor = 1000000.0
   let tez f =
-    let mutez = Int64.of_float( f *. 1000000.0 ) in
+    let mutez = Int64.of_float( f *. conversion_factor ) in
     assert (mutez >= Int64.one);
     match Tez.( *? ) Tez.one_mutez mutez with
     | Ok tz -> tz
     | _ -> failwith "Illegal Tez value"
+  let zero = Tez.zero
+  let to_float tz =
+    let mutez_f = Int64.to_float @@ Tez.to_mutez tz in
+    (mutez_f /. conversion_factor)
 end
 
-type failure_message = Insufficient_balance
-                     | Counter_mismatch
-                     | Invalid_receiver
-                     | Insufficient_fee
-                     | Reached_burncap
-                     | Reached_feecap
-                     | Unknown_failure of string
-type answer = Pending of oph
-            | Fail of failure_message
-
-type result = {
+type op_result = {
     block_hash : blockh;
     rpc_position : (int * int);
     balance_updates : Delegate.balance_updates;
@@ -58,22 +55,19 @@ type result = {
 type reason = Timeout
             | Skipped
             | Backtracked
-            | Reason of failure_message
-
-type error_message = RPC_error of {uri: string}
-                   | Unexpected_result
-                   | Unknown_error of string
+            | Reason of rejection_message
+            | Unknown_reason of string
 
 type status = Still_pending
-            | Accepted of result
+            | Accepted of op_result
             | Rejected of reason
             | Unprocessed
             | Missing
-            | Error of error_message
 
 type config = {
     port : int ref;
-    basedir : string ref
+    basedir : string ref;
+    debug_mode : bool ref
   }
 
 type fee_config = {
@@ -87,7 +81,8 @@ type fee_config = {
 
 let current_config = {
     port = ref 8732;
-    basedir = ref "/home/tezos/.tezos-client"
+    basedir = ref "/home/tezos/.tezos-client";
+    debug_mode = ref false
   }
 
 let current_fee_config = {
@@ -116,6 +111,8 @@ let make_context () =
     ~rpc_config:rpc_config
 
 let ctxt = ref (make_context ())
+let catch_error_f err = if !(current_config.debug_mode) then catch_trace err
+                        else catch_last_error err
 
 let make_fee_parameter () =
   let fp : fee_parameter = {
@@ -134,20 +131,9 @@ let fee_parameter = ref (make_fee_parameter ())
 let exception_handler =
   (function
   | Failure msg ->
-     fail (Exn (Failure msg))
+     Lwt.return_error [(Exn (Failure msg))]
   | exn ->
      Lwt.return @@ error_exn exn )
-
-let errors_of_strings =
-  Base.Map.of_alist_exn (module String)
-    [
-      ("The proposed fee .* are higher than the configured fee cap", Reached_feecap);
-      ("The proposed fee .* are lower than the fee that baker expect", Insufficient_fee);
-      ("The operation will burn .* which is higher than the configured burn cap", Reached_burncap);
-    ]
-
-let err_to_str = asprintf "%a" Error_monad.pp
-let env_err_to_str = asprintf "%a" Environment.Error_monad.pp
 
 let get_puk_from_alias name =
   Public_key_hash.find !ctxt name
@@ -155,9 +141,9 @@ let get_puk_from_alias name =
   | Ok pkh -> (
      Client_keys.get_key !ctxt pkh
      >>= function
-     | Ok (_, src_pk, _) -> Lwt.return_some src_pk
-     | _ -> Lwt.return_none )
-  | _ -> Lwt.return_none
+     | Ok (_, src_pk, _) -> Answer.return src_pk
+     | Error err -> catch_error_f err )
+  | Error err -> catch_error_f err
 
 let get_puk_from_hash pk_hash =
   Public_key_hash.of_source pk_hash
@@ -165,24 +151,24 @@ let get_puk_from_hash pk_hash =
   | Ok pkh -> (
     Client_keys.get_key !ctxt pkh
     >>= function
-    | Ok (_, src_pk, _) -> Lwt.return_some src_pk
-    | _ -> Lwt.return_none )
-  | _ -> Lwt.return_none
+    | Ok (_, src_pk, _) -> Answer.return src_pk
+    | Error err -> catch_error_f err)
+  | Error err -> catch_error_f err
 
 let get_pukh_from_alias name =
   Public_key_hash.find !ctxt name
   >>= function
-  | Ok pkh -> Lwt.return_some pkh
-  | _ -> Lwt.return_none
+  | Ok pkh -> Answer.return pkh
+  | Error err -> catch_error_f err
 
 let get_contract s =
   ContractAlias.get_contract !ctxt s
   >>= function
-  | Ok (_,v) -> Lwt.return_some v
+  | Ok (_,v) -> Answer.return v
   | Error _ -> (
     match Contract.of_b58check s with
-    | Ok v -> Lwt.return_some v
-    | Error _ -> Lwt.return_none )
+    | Ok v -> Answer.return v
+    | Error err -> catch_last_env_error err )
 
 let set_port p =
   (current_config.port) := p;
@@ -192,20 +178,24 @@ let set_basedir path =
   (current_config.basedir) := path;
   ctxt := make_context ()
 
+let set_debugmode flag =
+  (current_config.debug_mode) := flag;
+  ()
+
 let setup_remote_signer =
   Client_keys.register_signer (module Tezos_signer_backends.Unencrypted)
 
-let transfer amount src destination fees =
+let transfer amount src destination fee =
   setup_remote_signer;
   Client_keys.get_key !ctxt src
   >>= function
-  | Error _ -> Lwt.return @@ Fail Invalid_receiver
+  | Error err -> catch_error_f err
   | Ok (_, src_pk, src_sk) ->
      begin
        let ctxt_proto = new wrap_full !ctxt in
        Lwt.catch
          (fun () ->
-           transfer
+           Client_proto_context.transfer
              ctxt_proto
              ~chain:!ctxt#chain
              ~block:!ctxt#block
@@ -213,7 +203,7 @@ let transfer amount src destination fees =
              ~dry_run:false
              ~verbose_signing:false
              ~source:src
-             ~fee:fees
+             ~fee
              ~src_pk
              ~src_sk
              ~destination
@@ -222,46 +212,51 @@ let transfer amount src destination fees =
              ())
          exception_handler
        >>= fun res ->
-       let open Tezos_protocol_006_PsCARTHA.Protocol.Contract_storage in
        match res with
-       | Ok ((oph,_,_),_) -> Lwt.return @@ Pending oph
-       | Error ((Environment.Ecoproto_error Contract.Balance_too_low _) :: _)
-         -> Lwt.return @@ Fail Insufficient_balance
-       | Error ((Environment.Ecoproto_error Counter_in_the_past _) :: _)
-         -> Lwt.return @@ Fail Counter_mismatch
-       | Error ((Environment.Ecoproto_error Counter_in_the_future _):: _)
-         -> Lwt.return @@ Fail Counter_mismatch
-       | Error errs ->
-          let last_err = List.hd errs in
-          match last_err with
-          | None -> Lwt.return @@ Fail (Unknown_failure "Empty trace")
-          | Some err ->
-             begin
-               let err_str = err_to_str err in
-               let rec match_error l str = match l with
-                 | [] -> Fail (Unknown_failure err_str)
-                 | x::xs -> (
-                   let r = Str.regexp x in
-                   if string_match r err_str 0 then Fail (Base.Map.find_exn errors_of_strings x)
-                   else match_error xs str
-                 )
-               in
-               let keys = Base.Map.keys errors_of_strings in
-               Lwt.return @@ match_error keys err_str
-             end
+       | Ok ((oph,_,_),_) -> Answer.return oph
+       | Error err -> catch_error_f err
      end
 
-let catch_rpc_error errs =
-  match errs with
-  | (RPC_context.Not_found {uri; _}) ::_
-    | (RPC_context.Gone {uri;_}) ::_
-    | (RPC_context.Generic_error {uri;_}) ::_ ->
-     Lwt.return (Error (RPC_error {uri = Uri.to_string uri }))
-  | err :: _ -> Lwt.return (Error (Unknown_error (err_to_str err)))
-  | _ ->  Lwt.return (Error (Unknown_error "Empty trace"))
+let call_contract amount src destination ?entrypoint ?arg fee =
+  let open Answer in
+  (match Contract.is_implicit destination with
+   | None -> Answer.return ()
+   | Some _ -> Answer.fail Not_callable )
+  >>=? fun () ->
+  Client_keys.get_key !ctxt src
+  >>= function
+  | Error err -> catch_error_f err
+  | Ok (_, src_pk, src_sk) ->
+     begin
+       let ctxt_proto = new wrap_full !ctxt in
+       Lwt.catch
+         (fun () ->
+           Client_proto_context.transfer
+             ctxt_proto
+             ~chain:!ctxt#chain
+             ~block:!ctxt#block
+             ?confirmations:!ctxt#confirmations
+             ~dry_run:false
+             ~verbose_signing:false
+             ~source:src
+             ~fee
+             ~src_pk
+             ~src_sk
+             ~destination
+             ?entrypoint
+             ?arg
+             ~amount
+             ~fee_parameter: !fee_parameter
+             ())
+         exception_handler
+       >>= fun res ->
+       match res with
+       | Ok ((oph,_,_),_) -> Answer.return oph
+       | Error err -> catch_error_f err
+     end
 
 let get_result ((op, res) : 'kind contents_list * 'kind contents_result_list) (b,i,j) =
-  let rec cr : type kind. kind contents_and_result_list -> status =
+  let rec cr : type kind. kind contents_and_result_list -> status Answer.t =
     function
     | Single_and_result (Manager_operation {operation; _},
                          Manager_operation_result {operation_result;_}) ->
@@ -272,12 +267,15 @@ let get_result ((op, res) : 'kind contents_list * 'kind contents_result_list) (b
            match operation_result with
            | Failed (_, errs) -> (
               match errs with
-              | (Non_existing_contract _) ::_ -> Rejected (Reason Invalid_receiver)
-              | err :: _ -> Rejected (Reason (Unknown_failure (env_err_to_str err)))
-              | _ -> Rejected (Reason (Unknown_failure "Empty trace")))
+              | (Non_existing_contract _) ::_ ->
+                 Answer.return (Rejected (Reason Invalid_receiver))
+              | err :: _ ->
+                 let err_str = asprintf "%a" Environment.Error_monad.pp err in
+                 Answer.return (Rejected (Unknown_reason err_str))
+              | _ -> Answer.return (Rejected (Unknown_reason "Empty trace")))
            | Applied (Transaction_result r) ->
               begin
-                let res : result = {
+                let res : op_result = {
                     block_hash = b;
                     rpc_position = (i,j);
                     balance_updates = r.balance_updates;
@@ -289,11 +287,11 @@ let get_result ((op, res) : 'kind contents_list * 'kind contents_result_list) (b
                     big_map_diff = r.big_map_diff;
                     allocated_destination_contract = r.allocated_destination_contract
                   } in
-                Accepted res
+                Answer.return (Accepted res)
               end
-           | Backtracked ((Transaction_result _), _) -> Rejected Backtracked
-           | Skipped _ -> Rejected Skipped)
-         | _ -> Error Unexpected_result
+           | Backtracked ((Transaction_result _), _) -> Answer.return (Rejected Backtracked)
+           | Skipped _ -> Answer.return (Rejected Skipped))
+         | _ -> Answer.fail Unexpected_result
        end
     | Cons_and_result ((Manager_operation {operation;_} as op), (Manager_operation_result _ as res), rest) ->
        begin
@@ -301,11 +299,11 @@ let get_result ((op, res) : 'kind contents_list * 'kind contents_result_list) (b
          | Transaction _ -> cr @@ Single_and_result (op, res)
          | _ -> cr rest
        end
-    | _ -> (Error Unexpected_result)
+    | _ -> Answer.fail Unexpected_result
   in
   let contents_result_l = Apply_results.pack_contents_list op res
   in
-  Lwt.return @@ cr contents_result_l
+  cr contents_result_l
 
 let check_mempool oph =
   Alpha_block_services.Mempool.pending_operations
@@ -333,16 +331,14 @@ let check_mempool oph =
        in
        let pools_pending = applied @ branch_delayed_ops in
        let pools = (pools_pending, Still_pending)
-                   :: (refused_ops, Rejected (Reason (Unknown_failure "Unknown")))
+                   :: (refused_ops, Rejected (Unknown_reason "Unknown"))
                    :: (branch_refused_ops, Still_pending)
                    :: (branch_delayed_ops, Still_pending)
                    :: (unproc_ops, Unprocessed)
                    :: [] in
-       Lwt.return @@ find_op pools
+      Answer.return @@ find_op pools
      end
-  | Error errs -> match errs with
-                  | err :: _ -> Lwt.return (Error (Unknown_error (err_to_str err)))
-                  | _ -> Lwt.return (Error (Unknown_error ("Empty trace")))
+  | Error errs -> catch_error_f errs
 
 let query oph =
   let ctxt_proto = new wrap_full !ctxt in
@@ -370,10 +366,20 @@ let query oph =
               match Apply_results.kind_equal_list od.contents omd.contents with
               | Some Apply_results.Eq ->
                  get_result (od.contents, omd.contents) (block, i, j)
-              | None -> Lwt.return (Error Unexpected_result)
+              | None -> Answer.fail Unexpected_result
             end
-         | _ ->  Lwt.return (Error Unexpected_result)
+         | _ -> Answer.fail Unexpected_result
       )
-      | Error err -> catch_rpc_error err
+      | Error err -> catch_error_f err
      end
-  | Error err -> catch_rpc_error err
+  | Error err -> catch_error_f err
+
+let get_balance c =
+  let ctxt_proto = new wrap_full !ctxt in
+  Client_proto_context.get_balance
+    ctxt_proto
+    ~chain:ctxt_proto#chain
+    ~block:ctxt_proto#block c
+  >>= function
+  | Ok amount -> Answer.return amount
+  | Error err -> catch_error_f err
--- api/SyncAPIV0.mli
+++ api/SyncAPIV0.mli
@@ -1,5 +1,6 @@
 (** This API provides functions to interact with a Tezos node *)
 open Tezos_protocol_006_PsCARTHA.Protocol.Alpha_context
+open SyncAPIV0_error
 
 (** A public key of an account (implicit or originated)*)
 type puk
@@ -16,27 +17,24 @@ type oph
 (** Block hash *)
 type blockh
 
-(** Representation of Tezos tokens (tezzies) *)
+(** Representation of Tezos tokens (tez) *)
 module Tez_t : sig
   type t
+
+  (** [tez f] convert amount to the internal Tez representation
+      throws an exception if value is below 1 micro tez (0.000001)
+   *)
   val tez : float -> t
+
+  (** [zero] represents the value of 0 Tez *)
+  val zero : t
+
+  (** [to_float t] returns amount of tez in float represention *)
+  val to_float : t -> float
 end
 
-(** Provides information about why a transaction failed / is invalid *)
-type failure_message = Insufficient_balance
-                     | Counter_mismatch
-                     | Invalid_receiver
-                     | Insufficient_fee
-                     | Reached_burncap
-                     | Reached_feecap
-                     | Unknown_failure of string (** None of the above match - error list should be extended if this occurs *)
-
-(** Result of a transaction injection*)
-type answer = Pending of oph (** The operation hash of the successfully injected transaction *)
-            | Fail of failure_message (** An error with the respective {!type:failure_message} *)
-
 (** Result of an operation which has been successfully included into a block *)
-type result = {
+type op_result = {
     block_hash : blockh; (** Block in which transaction was included *)
     rpc_position : (int * int); (** The indices where the operation can be found within the block *)
     balance_updates : Delegate.balance_updates; (** List of balance updates *)
@@ -53,54 +51,52 @@ type result = {
 type reason = Timeout (** The transaction timed out and was removed from the Mempool (tbd if this case can be distinguished clearly from others) *)
             | Skipped (** The transaction was skipped due to a previously failed operation *)
             | Backtracked (** The transaction was backtracked due to a subsequently failed operation *)
-            | Reason of failure_message  (** The transaction failed due to {!type: failure_message} *)
-
-(** Errors which might occur during retrieval of transaction status (unrelated to the transaction result) *)
-type error_message = RPC_error of {uri: string} (** Error occured during RPC call *)
-                   | Unexpected_result (** The {!type:oph} did not refer to a transaction operation *)
-                   | Unknown_error of string (** Unknown error - error list should be extended if this occurs *)
+            | Reason of rejection_message  (** The transaction failed due to {!type: failure_message} *)
+            | Unknown_reason of string (** None of the above match - reason list should be extended if this occurs *)
 
 (** Status of an injected transaction *)
 type status = Still_pending (** Transaction hasn't been included yet (prevalidated, delayed or unprocessed) *)
-            | Accepted of result (** Transaction was included with {!type:result} *)
+            | Accepted of op_result (** Transaction was included with {!type:result} *)
             | Rejected of reason (** Transaction was rejected due to {!type:reason} *)
             | Unprocessed (** Transaction not yet prevalidated *)
             | Missing (** Transaction couldn't be found (tbd should this be timeout?) *)
-            | Error of error_message (** Status couldn't be retrieved due to {!type:error_message} *)
 
 (** [get_puk_from_alias s] expects an alias of an implicit account and returns
     the associated public key of the account.
     @param s alias of implicit account
     @return {!type:puk} the associated public key
 *)
-val get_puk_from_alias: string -> puk option Lwt.t
+val get_puk_from_alias: string -> puk Answer.t
 
 (** [get_puk_from_hash s] expects a public key hash as string and returns the associated
     public key of the account.
     @param s public key hash
     @return {!type:puk} the associated public key
  *)
-val get_puk_from_hash: string -> puk option Lwt.t
+val get_puk_from_hash: string -> puk Answer.t
 
 (** [get_pukh_from_alias s] expects an alias of an implicit account and returns 
     the associated public key hash.
     @param s alias of implicit account
     @return {!type:pukh} the associated public key hash
 *)
-val get_pukh_from_alias: string -> pukh option Lwt.t
+val get_pukh_from_alias: string -> pukh Answer.t
 
 (** [get_contract s] expects an alias, public key hash or contract hash as string
     and returns the associated contract representation.
     @param s alias, public key hash or contract hash
     @return {!type:contract} the associated contract representation
 *)
-val get_contract: string -> contract option Lwt.t
+val get_contract: string -> contract Answer.t
 
 (** [set_port p] specifies under which port the RPC interface of the node is
     reachable if the default (8732) does not apply.
     @param p the RPC port of the node
 *)
 val set_port: int -> unit
+
+(** [set_debugmode f] enable/disable the debug mode - in debug mode, the whole Tezos error trace is printed *)
+val set_debugmode : bool -> unit
 
 (** [set_basedir d] specifies the path of the tezos-client base directory
     (normally /home/<username>/.tezos-client/)
@@ -109,17 +105,32 @@ val set_port: int -> unit
 val set_basedir: string -> unit
 
 (** [transfer a src dst fee] injects a transfer transaction.
-    @param a the amount of tezzies to be transferred
+    @param a the amount of tez to transfer
     @param src the public key hash of the sender
     @param dst the contract representation of the receiver
-    @param fee the amount of fees to be paid for the baker
-    @return {!type:answer} the operation hash of the injected transaction or
-    error
+    @param fee the amount of fees to pay to the baker
+    @return {!type:oph} the operation hash of the injected transaction
 *)
-val transfer: Tez_t.t -> pukh -> contract -> Tez_t.t -> answer Lwt.t
+val transfer: Tez_t.t -> pukh -> contract -> Tez_t.t -> oph Answer.t
 
 (** [query op] retrieves the current status of an injected transaction
     @param op the operation hash of the injected transaction
-    @return {!type:status} a status or error
+    @return {!type:status} the status of the transaction
 *)
-val query : oph -> status Lwt.t
+val query : oph -> status Answer.t
+
+(** [get_balance c] returns the balance of a contract (implicit or originated)
+    @param f the contract representation of the target
+    @return {!type:Tez_t.t} the balance of the target contract
+*)
+val get_balance : contract -> Tez_t.t Answer.t
+
+(** [call_contract a src dst ?ep ?arg fee] calls a contract.
+    @param a amount of tez to transfer
+    @param src the public key hash of the caller
+    @param dst the contract representation of the callee
+    @param ?ep specifies the entrypoint - if not set, the default entrypoint will be called
+    @param ?arg argument passed to the contract's script (if needed)
+    @param fee the amount of fees to pay to the baker
+ *)
+val call_contract : Tez_t.t -> pukh -> contract -> ?entrypoint:string -> ?arg:string -> Tez_t.t -> oph Answer.t
