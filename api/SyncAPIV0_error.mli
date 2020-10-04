7c62bbf4abd653ed521785e71bbfd5bce8574a88
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2020-10-04 18:17:58 +0200

    Update current API specification

---
+++ api/SyncAPIV0_error.mli
@@ -0,0 +1,39 @@
+open Tezos_protocol_environment_006_PsCARTHA
+
+(** Provides information about why a transaction was rejected *)
+type rejection_message = Insufficient_balance
+                     | Counter_mismatch
+                     | Invalid_receiver
+                     | Insufficient_fee
+                     | Reached_burncap
+                     | Reached_feecap
+                     | Michelson_parser_error
+                     | Michelson_runtime_error
+
+type error = Rejection of rejection_message (** Injection failed due to {!type:rejection_message} *)
+           | RPC_error of {uri: string} (** Error occured during RPC call *)
+           | Node_connection_failed (** Connecting to the node fail *)
+           | Unexpected_result (** The {!type:oph} did not refer to a transaction operation *)
+           | Unknown_secret_key (** The secret key of the given account was not found *)
+           | Unknown_public_key (** The public key of the given account was not found *)
+           | Keys_not_found (** Unknown alias or no keys found for the given contract *)
+           | Wrong_contract_notation of string (** A malformed contract notation was given *)
+           | Not_callable (** The given contract is not originated *)
+           | Unknown of string (** Unknown error - error list should be extended if this occurs; used by debug_mode to return the Tezos error trace*)
+
+(** Simple error monad to handle improper input and Tezos errors*)
+module Answer : sig
+  type 'a t = ('a, error) Result.t Lwt.t
+  val return : 'a -> 'a t
+  val fail : error -> 'a t
+  val ( >>=? ) : 'a t -> ('a -> 'b t) -> 'b t
+end
+
+(** Error handling for Tezos Environment errors - only handles the top error on the Tezos error trace *)
+val catch_last_env_error : Environment.Error_monad.error list -> 'a Answer.t
+
+(** Error handling - only handles the top error on the Tezos error trace *)
+val catch_last_error : Error_monad.error list -> 'a Answer.t
+
+(** Error handling in debug mode - returns the entire Tezos error trace as string *)
+val catch_trace : Error_monad.error list -> 'a Answer.t
--- examples/test.ml
+++ examples/test.ml
@@ -1,108 +1,149 @@
 open Tezos_api
+open Tezos_client_006_PsCARTHA
    
 let command = ref "puk_alias"
 let port = ref 0
-let basedir = ref "/home/tezos/.tezos-client"
+let basedir = ref "/home/tamara/Studium/Tezos/project/tezos-project/sandbox"
+let debug = ref false
 
-let usage = "Usage: " ^ Sys.argv.(0) ^ " -c (puk_alias | puk_hash | pukh_alias | get_contr | tez | transfer | query)"
+let usage = "Usage: " ^ Sys.argv.(0) ^ " -c  (puk_alias | puk_hash | pukh_alias | get_contr | tez | transfer | query | balance | call)"
 let spec_list = [
     ("-c", Arg.Set_string command, ": specifies which command should be executed; default = " ^ !command);
     ("-p", Arg.Set_int port, ": specifies RPC port of the Tezos node; default =8732");
-    ("-d", Arg.Set_string basedir, ": specifies base directory of the Tezos client; default = /home/tezos/.tezos-client")
+    ("-d", Arg.Set_string basedir, ": specifies base directory of the Tezos client; default = /home/tezos/.tezos-client");
+    ("-v", Arg.Set debug, ": enables debug mode (prints the whole Tezos error trace)")
   ]
 
-let str_of_err err = match err with
-  | SyncAPIV0.Insufficient_balance -> "Insufficient_balance"
-  | SyncAPIV0.Insufficient_fee -> "Insufficient_fee"
-  | SyncAPIV0.Counter_mismatch -> "Counter_mismatch"
-  | SyncAPIV0.Invalid_receiver -> "Invalid_receiver"
-  | SyncAPIV0.Reached_burncap -> "Reached_burncap"
-  | SyncAPIV0.Reached_feecap -> "Reached_feecap"
-  | SyncAPIV0.Unknown_failure str -> str
+let str_of_err = function
+  | SyncAPIV0_error.Rejection Insufficient_balance -> "Insufficient_balance"
+  | SyncAPIV0_error.Rejection Insufficient_fee -> "Insufficient_fee"
+  | SyncAPIV0_error.Rejection Counter_mismatch -> "Counter_mismatch"
+  | SyncAPIV0_error.Rejection Invalid_receiver -> "Invalid_receiver"
+  | SyncAPIV0_error.Rejection Reached_burncap -> "Reached_burncap"
+  | SyncAPIV0_error.Rejection Reached_feecap -> "Reached_feecap"
+  | SyncAPIV0_error.Rejection Michelson_parser_error -> "Michelson_parser_error"
+  | SyncAPIV0_error.Rejection Michelson_runtime_error -> "Michelson_runtime_error"
+  | SyncAPIV0_error.RPC_error {uri} -> "RPC error at " ^ uri
+  | SyncAPIV0_error.Node_connection_failed -> "Node_connection_failed"
+  | SyncAPIV0_error.Unexpected_result -> "Unexpected_result"
+  | SyncAPIV0_error.Unknown_public_key -> "Unknown public key"
+  | SyncAPIV0_error.Unknown_secret_key -> "Unknown secret_key"
+  | SyncAPIV0_error.Keys_not_found -> "Keys not found"
+  | SyncAPIV0_error.Wrong_contract_notation s -> "Wrong_contract_notation " ^ s
+  | SyncAPIV0_error.Not_callable -> "Not_callable"
+  | SyncAPIV0_error.Unknown e -> e
 
-let str_of_status st = match st with
+let str_of_status = function
   | SyncAPIV0.Still_pending -> "Still_pending"
   | SyncAPIV0.Accepted _ -> "Accepted"
   | SyncAPIV0.Missing -> "Missing"
   | SyncAPIV0.Rejected (Reason r) -> (
-     let err_str = str_of_err r in
+     let err_str = str_of_err (Rejection r) in
      "Rejected - " ^ err_str)
+  | SyncAPIV0.Rejected (Unknown_reason s) -> s
   | SyncAPIV0.Rejected Timeout -> "Rejected - Timeout"
   | SyncAPIV0.Rejected Skipped -> "Rejected - Skipped"
   | SyncAPIV0.Rejected Backtracked -> "Rejected - Backtracked"
   | SyncAPIV0.Unprocessed -> "Unprocessed"
-  | SyncAPIV0.Error (Unknown_error str) -> str
-  | SyncAPIV0.Error Unexpected_result -> "Error - Unexpected_result"
-  | SyncAPIV0.Error RPC_error _ -> "Error - RPC"
 
 let run_puk_from_alias () =
   SyncAPIV0.get_puk_from_alias "dummyuser"
    >>= function
-  | Some _ -> print_endline "Found public key" ; Lwt.return 1
-  | None -> print_endline "Unknown key alias"; Lwt.return 0
+  | Ok _ -> print_endline "Found public key"; Lwt.return 1
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_puk_from_hash () =
   SyncAPIV0.get_puk_from_hash "tz1234567890"
   >>= function
-  | Some _ -> print_endline "Found public key" ; Lwt.return 1
-  | None -> print_endline "Invalid public key hash"; Lwt.return 0
+  | Ok _ -> print_endline "Found public key" ; Lwt.return 1
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_pukh_from_alias () =
   SyncAPIV0.get_pukh_from_alias "dummyuser"
   >>= function
-  | Some _ -> print_endline "Found public key hash" ; Lwt.return 1
-  | None -> print_endline "Unknown key alias"; Lwt.return 0
+  | Ok _ -> print_endline "Found public key hash" ; Lwt.return 1
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_get_contract () =
   SyncAPIV0.get_contract "dummyuser"
   (* SyncAPIV0.get_contract "tz1234567890" alternatively *)
   >>= function
-  | Some _ -> print_endline "Found contract" ; Lwt.return 1
-  | None -> print_endline "Invalid contract notation/Unknown contract"; Lwt.return 0
+  | Ok _ -> print_endline "Found contract" ; Lwt.return 1
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_transfer () =
   SyncAPIV0.get_pukh_from_alias "dummyuser"
   >>= function
-  | Some pkh_1 -> (
-    SyncAPIV0.get_contract "dummyuser2"
-    >>= function
-    | Some contr -> (
-      let amount = SyncAPIV0.Tez_t.tez 10.0 in
-      let fees = SyncAPIV0.Tez_t.tez 0.0001 in
-       SyncAPIV0.transfer amount pkh_1 contr fees
+  | Ok pukh -> (
+     SyncAPIV0.get_contract "dummyuser2"
+     >>= function
+     | Ok contr -> (
+       let amount = SyncAPIV0.Tez_t.tez 10.0 in
+       let fees = SyncAPIV0.Tez_t.tez 0.0001 in
+       SyncAPIV0.transfer amount pukh contr fees
        >>= function
-       | Fail err -> print_endline @@ str_of_err err; Lwt.return 0
-       | Pending _ -> print_endline "Operation is pending" ; Lwt.return 0
-    )
-    | None -> print_endline "Invalid/unknown destination contract"; Lwt.return 0 )
-  | None -> print_endline "Unknown source key alias"; Lwt.return 0
+       | Ok _ -> print_endline "Injection succesful" ; Lwt.return 1
+       | Error err -> print_endline @@ str_of_err err; Lwt.return 0 )
+     | Error err -> print_endline @@ str_of_err err; Lwt.return 0 )
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_query () =
   SyncAPIV0.get_pukh_from_alias "dummyuser"
   >>= function
-  | Some pkh_1 -> (
+  | Ok pkh_1 -> (
     SyncAPIV0.get_contract "dummyuser2"
     >>= function
-    | Some contr -> (
+    | Ok contr -> (
       let amount = SyncAPIV0.Tez_t.tez 10.0 in
       let fees = SyncAPIV0.Tez_t.tez 0.0001 in
        SyncAPIV0.transfer amount pkh_1 contr fees
        >>= function
-       | Fail err -> print_endline @@ str_of_err err; Lwt.return 0
-       | Pending oph -> (
+       | Error err -> print_endline @@ str_of_err err; Lwt.return 0
+       | Ok oph -> (
          SyncAPIV0.query oph
-         >>= fun st ->
-         print_endline @@ str_of_status st;
-         Lwt.return 1
-       ))
-    | None -> print_endline "Invalid/unknown destination contract"; Lwt.return 0 )
-  | None -> print_endline "Unknown source key alias"; Lwt.return 0
+         >>= function
+         | Ok st -> print_endline @@ str_of_status st; Lwt.return 1
+         | Error err -> print_endline @@ str_of_err err; Lwt.return 0
+    ))
+    | Error err -> print_endline @@ str_of_err err; Lwt.return 0)
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let run_tez () =
   let eq_classes = [1.0; 0.000001; 0.0000001] in
   let f = (fun tz -> print_endline @@ string_of_float tz; SyncAPIV0.Tez_t.tez tz) in
   let _ = List.map f eq_classes in
   Lwt.return 1
+
+let run_get_balance () =
+  SyncAPIV0.get_contract "dummyuser"
+  >>= function
+  | Ok contr -> (
+    SyncAPIV0.get_balance contr
+    >>= function
+    | Ok tz -> (
+      let tz_str = string_of_float @@ SyncAPIV0.Tez_t.to_float tz in
+      print_endline (tz_str ^ " " ^ Client_proto_args.tez_sym); Lwt.return 1
+    )
+    | Error err -> print_endline @@ str_of_err err; Lwt.return 0 )
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
+
+let run_call_contract () =
+  SyncAPIV0.get_pukh_from_alias "dummyuser"
+  >>= function
+  | Ok pukh -> (
+     SyncAPIV0.get_contract "KT123456789"
+     >>= function
+     | Ok contr -> (
+       let amount = SyncAPIV0.Tez_t.zero in
+       let fees = SyncAPIV0.Tez_t.tez 0.001 in
+       let entrypoint = "someEntrypoint" in
+       let arg = "Some parameters" in
+       SyncAPIV0.call_contract amount pukh contr ~entrypoint ~arg fees
+       >>= function
+       | Ok _ -> print_endline "Injection succesful"; Lwt.return 1
+       | Error err -> print_endline @@ str_of_err err; Lwt.return 0 )
+     | Error err -> print_endline @@ str_of_err err; Lwt.return 0 )
+  | Error err -> print_endline @@ str_of_err err; Lwt.return 0
 
 let main =
   Arg.parse
@@ -111,6 +152,7 @@ let main =
     usage;
   if !port != 0 then SyncAPIV0.set_port !port;
   if !basedir <> "" then SyncAPIV0.set_basedir !basedir;
+  if !debug then SyncAPIV0.set_debugmode true;
   if !command = "puk_alias" then run_puk_from_alias ()
   else if !command = "puk_hash" then run_puk_from_hash ()
   else if !command = "pukh_alias" then run_pukh_from_alias ()
@@ -118,10 +160,10 @@ let main =
   else if !command = "query" then run_query ()
   else if !command = "get_contr" then run_get_contract ()
   else if !command = "tez" then run_tez ()
+  else if !command = "balance" then run_get_balance ()
+  else if !command = "call" then run_call_contract ()
   else (print_endline "Unknown command" ; Lwt.return 0)
-  >>= fun retcode -> Lwt.return retcode 
+  >>= fun retcode -> Lwt.return retcode
 
 let () =
   Stdlib.exit @@ Lwt_main.run main
-
-  
