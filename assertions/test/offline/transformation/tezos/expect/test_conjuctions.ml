d820f7a5c1a9bb29fc13ec86bb39d7a643aa711b
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-01-05 16:35:59 +0100

    Add unit tests for tezos transformation

---
+++ assertions/test/offline/transformation/tezos/expect/test_conjuctions.ml
@@ -0,0 +1,39 @@
+open Core
+open Parsing.Lex_and_parse
+
+let%expect_test "break conjunctions" =
+  parse_contract
+    {|(entrypoint _
+        (if (and a (and b c))
+          (assert true)))
+      (entrypoint _
+        (if (and (and a b) c)
+          (assert true)))
+
+|}
+  |> Transformation.transform
+  |> Transformation.print_transformation ;
+  [%expect
+    {|
+    AST
+    └──Entrypoint: %default
+      └──Pattern: Wildcard
+    └──Assertion: If
+      └──Expr: Id:a
+      └──Assertion: If
+        └──Expr: Id:b
+        └──Assertion: If
+          └──Expr: Id:c
+          └──Assertion: Assert
+            └──Expr: Bool:false
+    AST
+    └──Entrypoint: %default
+      └──Pattern: Wildcard
+    └──Assertion: If
+      └──Expr: Id:a
+      └──Assertion: If
+        └──Expr: Id:b
+        └──Assertion: If
+          └──Expr: Id:c
+          └──Assertion: Assert
+            └──Expr: Bool:false|}]
