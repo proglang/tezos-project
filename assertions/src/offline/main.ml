266a1568abebff76da6c49837191358b388c2ecd
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-01-13 18:25:53 +0100

    Fix test programm and rename it to main

---
+++ assertions/src/offline/main.ml
@@ -0,0 +1,15 @@
+open Parsing.Lex_and_parse
+
+(* Checks if given list is sorted *)
+let example_contract = {| 
+(entrypoint (pair (l: (list int)) _)
+  (forall (n : int) 
+    (forall (m: int)
+      (if (and (lt n (size l)) (lt m (size l)))
+        (if (gt n m)
+          (assert (gt (nth l n) (nth l m))))))))|}
+
+(* The example contract is first parsed and then transformed by the backend *)
+let () = parse_contract example_contract
+         |> Transformation.transform
+         |> Transformation.print_transformation
