e07d74dcc7ddcd8aef6d9bb64976a6a9d6068732
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-02-09 18:27:01 +0100

    Add missing files...

---
+++ assertions/test/offline/tezos/typecheck/alcotest/test_helpers.ml
@@ -0,0 +1,17 @@
+open Lwt.Infix
+
+let lwt_check_raises (exp_s : string option) f =
+  Lwt.catch
+    (fun () -> f () >|= fun () -> `Ok)
+    (function e -> Lwt.return @@ `Error e)
+  >|= function
+  | `Ok -> Alcotest.fail "No exception was thrown"
+  | `Error (Failure s) ->
+     begin
+       match exp_s with
+       | Some exs ->
+          if String.equal s exs then Alcotest.(check pass) "Correct exception" () ()
+          else Alcotest.fail ("Caught unexpected exception: \n" ^ s)
+       | None -> Alcotest.(check pass) "Correct exception" () ()
+     end
+  | `Error _ -> Alcotest.fail "Incorrect exception was thrown"
---
+++ assertions/test/offline/tezos/typecheck/alcotest/test_helpers.mli
@@ -0,0 +1,1 @@
+val lwt_check_raises : string option -> (unit -> unit Lwt.t) -> unit Lwt.t
