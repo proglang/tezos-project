20346c85aab7eff61843a76bd4e360a8c986840d
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-02-09 15:54:25 +0100

    Add tests for pattern matching

--- assertions/test/offline/tezos/typecheck/alcotest/test_typecheck_types.ml
+++ assertions/test/offline/tezos/typecheck/alcotest/test_typecheck_types.ml
@@ -29,7 +29,7 @@ let lwt_check_raises (exp_s : string option) f =
      end
   | `Error _ -> Alcotest.fail "Incorrect exception was thrown"
 
-let test_match type_s =
+let test_type_match type_s =
   let code = Printf.sprintf ("(entrypoint (x: %s) (assert true))") type_s in
   generate_contract type_s;
   typecheck
@@ -37,11 +37,44 @@ let test_match type_s =
     file_path ()
   >|= (fun () -> Alcotest.(check pass) type_s () ())
 
+let test_pattern_match type_s pattern_s =
+  let code = Printf.sprintf ("(entrypoint %s (assert true))") pattern_s in
+  generate_contract type_s;
+  typecheck
+    code
+    file_path ()
+  >|= (fun () -> Alcotest.(check pass) pattern_s () ())
+
+let pattern_match_test_cases =
+  let open Alcotest_lwt in
+  List.map
+    ~f:(fun (type_s, pattern_s) ->
+      test_case ("Pattern match: " ^ pattern_s) `Quick (fun _ () -> test_pattern_match type_s pattern_s))
+    [
+      ("(list int)", "nil"); ("(list int)", "_"); ("(list int)", "(cons _ _)");
+      ("(list int)", "(cons (x : int) (xs : (list int)))");
+      ("(list int)", "(cons (x : int) nil)");
+      ("(list int)", "(cons (x : int) _ )");
+      ("(list int)", "(cons _ (xs : list int))");
+      ("(option bool)", "none"); ("(option bool)", "_");
+      ("(option bool)", "(some _)");
+      ("(option bool)", "(some (x : bool))");
+      ("(or mutez string)", "_");
+      ("(or mutez string)", "(left _)");
+      ("(or mutez string)", "(left (x : mutez))");
+      ("(or mutez string)", "(right _)");
+      ("(or mutez string)", "(right (x : string))");
+      ("(pair address bytes)", "_"); ("(pair address bytes)", "(pair _ _)");
+      ("(pair address bytes)", "(pair (x : address) _)");
+      ("(pair address bytes)", "(pair _ (x : bytes))");
+      ("(pair address bytes)", "(pair (x : address) (y : bytes))");
+    ]
+
 let type_match_test_cases =
   let open Alcotest_lwt in
   List.map
     ~f:(fun type_s ->
-      test_case ("Type match: " ^ type_s) `Quick (fun _ () -> test_match type_s))
+      test_case ("Type match: " ^ type_s) `Quick (fun _ () -> test_type_match type_s))
     [
      "int"; "bool"; "string"; "mutez"; "bytes"; "nat"; "address"; "chain_id";
      "key"; "key_hash"; "operation"; "timestamp"; "unit"; "(list int)";
@@ -55,5 +88,6 @@ let () =
   Lwt_main.run
   @@ run "Typechecker"
        [
-         ("Type matches", type_match_test_cases)
+         ("Type matches", type_match_test_cases);
+         ("Pattern matches", pattern_match_test_cases)
        ]
