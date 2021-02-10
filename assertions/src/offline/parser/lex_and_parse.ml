c647df83436cd5242890123fb7fdae8453b9df13
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-02-10 12:34:45 +0100

    Bugfix **TEMPORARY** -> raise syntax error exception instead of returning empty list

--- assertions/src/offline/parser/lex_and_parse.ml
+++ assertions/src/offline/parser/lex_and_parse.ml
@@ -1,32 +1,20 @@
 open Core
 open Lexing
 
-let print_position outx lexbuf =
+let print_position lexbuf =
   let pos = lexbuf.lex_curr_p in
-  fprintf outx "%s:%d:%d" pos.pos_fname
+  Fmt.str "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
+(* TODO: return Error monad instead raise exception *)
 let parse_with_error lexbuf =
   try Parser.main Lexer.read lexbuf with
   | Lexer.SyntaxError msg ->
-    fprintf stderr "%a: %s\n" print_position lexbuf msg;
-    None
+     let error_msg = Fmt.str "%s: %s@." (print_position lexbuf) msg in
+     failwith error_msg
   | Parser.Error ->
-    fprintf stderr "%a: syntax error\n" print_position lexbuf;
-    None
-
-(*
-let rec rec_parse_and_print lexbuf =
-  match parse_with_error lexbuf with
-  | Some x ->
-    Pp_ast.pp_ast Fmt.stdout x;
-    rec_parse_and_print lexbuf
-  | None -> ()
-
-let parse_and_print input_str =
-  let lexbuf = Lexing.from_string input_str in
-  rec_parse_and_print lexbuf
-*)
+     let error_msg = Fmt.str "%s: syntax error\n" (print_position lexbuf) in
+     failwith error_msg
 
 let maybe_pprint_ast a ~verbose =
   if verbose then (Pp_ast.pp_ast Fmt.stdout a; ())
