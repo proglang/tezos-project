abfc5ce0475bb1a9c229310a925f3bc9c0819c5c
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-02-28 23:02:27 +0100

    Maybeprint function for ep-assertion mapping

--- assertions/src/offline/tezos/check_and_compile.ml
+++ assertions/src/offline/tezos/check_and_compile.ml
@@ -1,5 +1,10 @@
+let maybe_pprint_ep_mapping ~verbose mapping =
+  if verbose then Type_checker.pp_ep_assertion_map Fmt.stdout mapping;
+  Lwt.return mapping
 
 let check_and_compile dao_t t_asts ~verbose =
   Tezos_ast.cast t_asts ~verbose
   |> Type_checker.type_check dao_t
+  >>= fun mapping ->
+  maybe_pprint_ep_mapping ~verbose mapping
   >>= fun _ -> return_unit
--- assertions/src/offline/tezos/typecheck/type_checker.mli
+++ assertions/src/offline/tezos/typecheck/type_checker.mli
@@ -4,3 +4,4 @@ open Tezos_ast.Ast
 module EntrypointAssertionMapping : Map.S with type key = Union_path.union_path
 
 val type_check : dao_type -> ast list -> ast EntrypointAssertionMapping.t Lwt.t
+val pp_ep_assertion_map : Format.formatter -> ast EntrypointAssertionMapping.t -> unit
