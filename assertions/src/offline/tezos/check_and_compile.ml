3dacbc029cf6e281eb07a7090ec7f2503e79ecda
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-03-01 21:18:14 +0100

    Configure API in backend with CLI args

--- assertions/src/offline/tezos/check_and_compile.ml
+++ assertions/src/offline/tezos/check_and_compile.ml
@@ -1,12 +1,19 @@
 open Cli_args
+open SyncAPIV1
 
 let maybe_pprint_ep_mapping ~verbose mapping =
   let open Type_checker.Entrypoint_mapping in
   if verbose then pp_mapping Fmt.stdout mapping;
   Lwt.return mapping
 
+let configure_api port basedir v =
+  if Option.is_some port then Api.set_port @@ Option.get port;
+  if Option.is_some basedir then Api.set_basedir @@ Option.get basedir;
+  if Option.is_some v then Api.set_debugmode @@ Option.get v
+
 let check_and_compile args t_asts =
   let verbose = args.verbose in
+  configure_api args.node_port args.node_basedir args.tezos_api_verbose;
   Tezos_ast.cast t_asts ~verbose
   |> Type_checker.type_check args.dao_contract
   >>= fun mapping ->
