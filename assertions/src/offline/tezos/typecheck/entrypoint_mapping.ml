a55e644d4f7376e0bc500fb6933ff20b0ffea802
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-03-01 11:20:28 +0100

    Add expect unit tests for type checker; verify the returned mapping

--- assertions/src/offline/tezos/typecheck/entrypoint_mapping.ml
+++ assertions/src/offline/tezos/typecheck/entrypoint_mapping.ml
@@ -7,10 +7,8 @@ module EntrypointAssertionMapping =
 
 let pp_mapping ppf m =
   let bindings = EntrypointAssertionMapping.bindings m in
-  let num_bindings = List.length bindings in
-  List.iteri
-    (fun i (p, ({entrypoint = ep; _}: Ast.ast)) ->
+  List.iter
+    (fun (p, ({entrypoint = ep; _}: Ast.ast)) ->
       Fmt.pf ppf "%a:\n" Union_path.pp p;
-      if i < num_bindings then (Fmt.pf ppf "%a\n" pp_ast_entrypoint ep)
-      else (Fmt.pf ppf "%a;\n" pp_ast_entrypoint ep))
-    bindings;
+      Fmt.pf ppf "%a\n" pp_ast_entrypoint ep)
+    bindings
--- assertions/test/offline/tezos/typecheck/expect/test_mapping.ml
+++ assertions/test/offline/tezos/typecheck/expect/test_mapping.ml
@@ -1,6 +1,9 @@
 open Core
 open Parsing.Lex_and_parse
 open Dao_type
+open Format
+
+let tz_code = asprintf "parameter %s; storage unit; code {UNIT ; NIL operation ; PAIR}"
 
 let exec tz tza =
   let open Type_checker.Entrypoint_mapping in
@@ -11,14 +14,119 @@ let exec tz tza =
   |> Lwt_main.run
   |> pp_mapping Fmt.stdout
 
-let%expect_test "test" =
+let%expect_test "Tz w/ tags; assertions w/o tags" =
   exec
-    {|parameter int; storage unit; code {UNIT ; NIL operation ; PAIR}|}
-    {|(entrypoint %A (i : int)
-        (assert true))|};
+    (tz_code "(or (int %A) (bool %B))")
+    {|(entrypoint (left (a: int)) (assert true))
+      (entrypoint (right (b: bool)) (assert true))|};
   [%expect
     {|
-    T:
+    RT:
+    Entrypoint: %default
+    └──Pattern: Right
+      └──Pattern: Id:b
+        └──Type: Bool_t
+    LT:
+    Entrypoint: %default
+    └──Pattern: Left
+      └──Pattern: Id:a
+        └──Type: Int_t|}]
+
+ let%expect_test "Tz w/o tags; assertions w/ tags" =
+  exec
+    (tz_code "(or int bool)")
+    {|(entrypoint %A (a: int) (assert true))
+      (entrypoint %B (b: bool) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %B
+    └──Pattern: Id:b
+      └──Type: Bool_t
+    LT:
     Entrypoint: %A
-    └──Pattern: Id:i
+    └──Pattern: Id:a
+      └──Type: Int_t|}]
+
+ let%expect_test "Tz w/o tags; assertions w/o tags" =
+  exec
+    (tz_code "(or int bool)")
+    {|(entrypoint (left (a: int)) (assert true))
+      (entrypoint (right (b: bool)) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %default
+    └──Pattern: Right
+      └──Pattern: Id:b
+        └──Type: Bool_t
+    LT:
+    Entrypoint: %default
+    └──Pattern: Left
+      └──Pattern: Id:a
+        └──Type: Int_t|}]
+
+ let%expect_test "Tz w/ tags; assertions w/ tags" =
+  exec
+    (tz_code "(or (int %A) (bool %B))")
+    {|(entrypoint %A (a: int) (assert true))
+      (entrypoint %B (b: bool) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %B
+    └──Pattern: Id:b
+      └──Type: Bool_t
+    LT:
+    Entrypoint: %A
+    └──Pattern: Id:a
+      └──Type: Int_t|}]
+
+ let%expect_test "Mixed tags" =
+  exec
+    (tz_code "(or (int %A) bool)")
+    {|(entrypoint %A (a: int) (assert true))
+      (entrypoint (right (b: bool)) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %default
+    └──Pattern: Right
+      └──Pattern: Id:b
+        └──Type: Bool_t
+    LT:
+    Entrypoint: %A
+    └──Pattern: Id:a
+      └──Type: Int_t|}]
+
+ let%expect_test "Unambiguous" =
+  exec
+    (tz_code "(or (int %A) (int %B))")
+    {|(entrypoint %A (a: int) (assert true))
+      (entrypoint %B (b: int) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %B
+    └──Pattern: Id:b
+      └──Type: Int_t
+    LT:
+    Entrypoint: %A
+    └──Pattern: Id:a
+      └──Type: Int_t|}]
+
+let%expect_test "Non-matching tags but unambiguous types" =
+  exec
+    (tz_code "(or (int %A) (bool %B))")
+    {|(entrypoint %C (a: int) (assert true))
+      (entrypoint %D (b: bool) (assert true))|};
+  [%expect
+    {|
+    RT:
+    Entrypoint: %D
+    └──Pattern: Id:b
+      └──Type: Bool_t
+    LT:
+    Entrypoint: %C
+    └──Pattern: Id:a
       └──Type: Int_t|}]
