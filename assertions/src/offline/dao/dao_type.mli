d3f85296a8825b5ee7a3706dacab0bf90cdb73a3
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2021-03-03 20:45:14 +0100

    Add comments to DAO interface

--- assertions/src/offline/dao/dao_type.mli
+++ assertions/src/offline/dao/dao_type.mli
@@ -1,3 +1,9 @@
+(** This module defines data access objects used to retrieve the contract code
+    The contract code can either be given as an address on the chain, a file path
+    or a string (mainly for testing purposes).
+    How the code is retrieved and represented is abstracted away and depends on the backend.
+*)
+
 type dao_type = DAO_File of string
               | DAO_Chain of string
-              | DAO_String of string (* For tests *)
+              | DAO_String of string
