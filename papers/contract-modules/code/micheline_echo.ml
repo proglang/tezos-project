2a66fb8b419da04e160c853e293767319acba171
Peter Thiemann <peter.thiemann@gmail.com>
2021-07-26 18:25:37 +0200

    print more info

--- papers/contract-modules/code/micheline_echo.ml
+++ papers/contract-modules/code/micheline_echo.ml
@@ -11,15 +11,16 @@ let return x = (x, [])
 
 let rec print_node n =
   match n with
-  | Micheline.Int (_loc, _i)->
-    print_string "int "
-  | Micheline.Bytes (_loc, _b) ->
-    print_string "bytes "
+  | Micheline.Int (_loc, i)->
+    print_string "int "; print_string (Z.to_string i^" ")
+  | Micheline.Bytes (_loc, b) ->
+    print_string "bytes "; print_string (Bytes.to_string b)
   | Micheline.String (_loc, s) ->
     print_string ("string \""^s^"\"")
-  | Micheline.Prim (_loc, s, ns, _) ->
-    (print_string ("prim \""^s^"\"(") ; List.iter ns ~f:print_node ;
-    print_string (") "); Out_channel.newline stdout)
+  | Micheline.Prim (_loc, s, ns, ann) ->
+    (print_string ("prim \""^s^"\" ") ; List.iter ann ~f:(fun s -> print_string (s^" ")) ; 
+     print_string " ("; List.iter ns ~f:print_node ;
+     print_string (") "); Out_channel.newline stdout)
   | Micheline.Seq (_loc, ns) ->
     (print_string ("seq (") ; List.iter ns ~f:print_node ; print_string (") "))
 
@@ -31,8 +32,9 @@ let run_file filename =
       let* nodes = Micheline_parser.parse_toplevel tokens in
       let printable_nodes =
         List.map nodes ~f:(map_node (fun _ -> {Micheline_printer.comment=None}) (fun x -> x)) in
-      let () = List.iter printable_nodes ~f:(Micheline_printer.print_expr Format.std_formatter) in
+      (* let () = List.iter printable_nodes ~f:(Micheline_printer.print_expr Format.std_formatter) in *)
       let () = Out_channel.newline stdout; List.iter printable_nodes ~f:print_node in
+      let _adapted = Michel_adapt.conv_toplevel printable_nodes in
       return ())
   in ()
 
