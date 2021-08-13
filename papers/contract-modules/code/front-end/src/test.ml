09105d562fed60a7cb8bdbc660c3283ab56cf2ab
doanha86 <33692612+doanha86@users.noreply.github.com>
2021-08-13 15:56:36 +0200

    Add files via upload

--- papers/contract-modules/code/front-end/src/test.ml
+++ papers/contract-modules/code/front-end/src/test.ml
@@ -11,14 +11,11 @@ let x = parse_contract example
 val x : Contract_module_t.contract_module_ast list =
   [{Parsing.Contract_module_t.contract = "Auction";
     body =
-     `Cons
-       ({Parsing.Contract_module_t.entrypoint = ("bid", `Ident ("n", `Int_t));
-         error = `Cons ("closed", `Cons ("close action", `Nil))},
-        `Cons
-          ({Parsing.Contract_module_t.entrypoint = ("close", `Wildcard);
-            error = `Cons ("not owner", `Nil)},
-           `Nil))}]
-
+     [{Parsing.Contract_module_t.entrypoint = ("bid", `Ident ("n", `Int_t));
+       errors = ["closed"; "close action"]};
+      {Parsing.Contract_module_t.entrypoint =
+        ("close", `Ident ("s", `String_t));
+       errors = ["not owner"]}]}]
 
 **)
 
@@ -28,44 +25,37 @@ raises "not owner") end)|}
 
 let y = parse_contract example_1
 
-(** val y : Contract_module_t.contract_module_ast list =
+(** 
+val y : Contract_module_t.contract_module_ast list =
   [{Parsing.Contract_module_t.contract = "Auction";
     body =
-     `Cons
-       ({Parsing.Contract_module_t.entrypoint = ("bid", `Ident ("n", `Int_t));
-         error = `Cons ("closed", `Cons ("close action", `Nil))},
-        `Cons
-          ({Parsing.Contract_module_t.entrypoint = ("close", `Wildcard);
-            error = `Cons ("not owner", `Nil)},
-           `Nil))}]
+     [{Parsing.Contract_module_t.entrypoint = ("bid", `Ident ("n", `Int_t));
+       errors = ["closed"; "close action"]};
+      {Parsing.Contract_module_t.entrypoint = ("close", `Wildcard);
+       errors = ["not owner"]}]}]
+
 **)
 
+let example_2 = {|(contract type Auction = sig (paid entrypoint bid (left (right (n : int)))
+raises "closed" | "close action") (entrypoint close (_)
+raises "not owner") end)|}
+
+let z = parse_contract example_2
+
+(** 
+val z : Contract_module_t.contract_module_ast list =
+  [{Parsing.Contract_module_t.contract = "Auction";
+    body =
+     [{Parsing.Contract_module_t.entrypoint =
+        ("bid", `Left (`Right (`Ident ("n", `Int_t))));
+       errors = ["closed"; "close action"]};
+      {Parsing.Contract_module_t.entrypoint = ("close", `Wildcard);
+       errors = ["not owner"]}]}]
+**)
 
 let () = parse_and_print example
 
-let () = print_past ({Parsing.Contract_module_t.contract = "Auction";
-    body =
-     `Cons
-       ({Parsing.Contract_module_t.entrypoint = ("bid", `Ident ("n", `Int_t));
-         error = `Cons ("closed", `Cons ("close action", `Nil))},
-        `Cons
-          ({Parsing.Contract_module_t.entrypoint = ("close", `Wildcard);
-            error = `Cons ("not owner", `Nil)},
-           `Nil))})
-(** AST
-└──Contract: %Auction
-    └──Entrypoint: %bid
-      └──Pattern: Id:n
-        └──Type: Int_t
-    └──Error: %closed
-      └──Error: %close action
-        └──Error_list: %Nil
-      └──Entrypoint: %close
-        └──Pattern: Id:s
-          └──Type: String_t
-      └──Error: %not owner
-        └──Error_list: %Nil
-    └──Entrypoint_list: %Nil
+(** 
 **)
 
 
@@ -75,5 +65,10 @@ let () =
   |> List.iter print_string
   |> print_newline
 
-(** {"contract":"Auction","body":["Cons",[{"entrypoint":["bid",["Ident",["n","Int_t"]]],"error":["Cons",["closed",["Cons",["close action","Nil"]]]]},["Cons",[{"entrypoint":["close",["Ident",["s","String_t"]]],"error":["Cons",["not owner","Nil"]]},"Nil"]]]]} **)
+(** {"contract":"Auction",
+     "body":[[{"entrypoint":["bid",["Ident",["n","Int_t"]]],
+              "errors":[["closed","close action"]]},
+              {"entrypoint":["close",["Ident",["s","String_t"]]],
+               "errors":[["not owner"]]}]]}
+ **)
 
