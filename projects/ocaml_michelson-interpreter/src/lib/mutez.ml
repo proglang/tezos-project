7a10cc35b6085822c2034531cdf3f8dc0d7048f4
JoAllg <joshua@kosmosvogel.de>
2021-10-18 18:20:18 +0200

    mutez libary fully implemented except arithmetic operations with integers

--- projects/ocaml_michelson-interpreter/src/lib/mutez.ml
+++ projects/ocaml_michelson-interpreter/src/lib/mutez.ml
@@ -5,12 +5,15 @@ Mutez (micro-Tez) are internally represented by 64-bit signed integers.
 These are restricted to prevent creating a negative amount of mutez.
 Instructions are limited to prevent overflow and mixing them with other numerical types by mistake.
 They are also mandatorily checked for overflows.
+https://tezos.gitlab.io/alpha/michelson.html#operations-on-mutez
 *)
 
 module Mutez : sig
     type t
     exception Overflow of string * string * string
     exception NegativeMutez of string * string * string
+    val min_t : t
+    val max_t : t
     val of_int : int -> t
     val of_int64 : int64 -> t
     val of_Zt : Z.t -> t
@@ -21,59 +24,58 @@ module Mutez : sig
     val to_string : t -> string
     val add : t -> t -> t
     val sub : t -> t -> t
-    val mul : t -> t -> t
-    val ediv : t -> t -> (t * t) option
-    val value : t -> Z.t
+    val mul : 'a -> 'a -> t
+    val ediv : t -> 'a -> (t * t) option
+    val compare : t -> t -> int
     (*val format : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'b -> 'a -> unit*)
   end = struct
-    type t = { value : Z.t; }
+    type t = Z.t
 
     exception Overflow of string * string * string
     exception NegativeMutez of string * string * string
 
     let min_t = Z.of_int 0
     let max_t = Z.of_string "9223372036854775807"
+    let check_range (value : Z.t) : unit =
+      if (Z.lt value min_t || Z.gt value  max_t) then
+        raise (Overflow ("Mutez.of_string: Overflow", Z.to_string value, ""))
 
-    let of_int (x : int) = { value = Z.of_int x }
-    let of_int64 (x : int64) = { value = Z.of_int64 x }
-    let of_Zt (x : Z.t) = { value = x }
-    let of_string (s : string) =
-      let value = Z.of_string s in
-      if (value < min_t || value > max_t) then
-        raise (Overflow ("Mutez.of_string: Overflow", s, ""))
-      else { value }
+    let of_int (x : int) = check_range (Z.of_int x); Z.of_int x
+    let of_int64 (x : int64) = check_range (Z.of_int64 x); Z.of_int64 x
+    let of_Zt (x : Z.t) = check_range x; x
+    let of_string (s : string) = check_range (Z.of_string s); Z.of_string s
 
-    let to_int (x : t) = Z.to_int x.value (* may raise Z.Overflow *)
-    let to_int64 (x : t) = Z.to_int64 x.value
-    let to_Zt (x : t) = x.value
-    let to_string (x : t) = Z.to_string x.value
+    let to_int (x : t) : int = Z.to_int x (* may raise Z.Overflow *)
+    let to_int64 (x : t) : int64 = Z.to_int64 x
+    let to_Zt (x : t) : Z.t = x
+    let to_string (x : t) : string = Z.to_string x
 
     let add (x : t) (y : t) : t =
-      let value = Z.add x.value y.value in
-      if (value > max_t) then
-        raise (Overflow ("Mutez.add: Overflow", Z.to_string(x.value), Z.to_string(y.value)))
-      else { value }
+      let value = Z.add x y in
+      if (Z.gt value max_t) then
+        raise (Overflow ("Mutez.add: Overflow", Z.to_string(x), Z.to_string(y)))
+      else value
 
     let sub (x : t) (y : t) : t =
-      let value = Z.sub x.value y.value in
-      if (value < min_t) then
-        raise (NegativeMutez ("Mutez.sub: Mutez can not be negative", Z.to_string(x.value), Z.to_string(y.value)))
-      else { value }
+      let value = Z.sub x y in
+      if (Z.lt value min_t) then
+        raise (NegativeMutez ("Mutez.sub: Mutez can not be negative", Z.to_string(x), Z.to_string(y)))
+      else value
 
-    let mul (x : t) (y : t) : t =
-      let value = Z.mul x.value y.value in
-      if (value > max_t) then
-        raise (Overflow ("Mutez.mul: Overflow", Z.to_string(x.value), Z.to_string(y.value)))
-      else { value }
+    let mul x y : t =
+      let value = Z.mul (Mutez.of_Zt x) (Mutez.of_Zt y) in
+      if (Z.gt value max_t) then
+        raise (Overflow ("Mutez.mul: Overflow", Z.to_string(x), Z.to_string(y)))
+      else value
 
-    let ediv (x : t) (y : t) : (t * t) option =
+    let ediv (x : t) y : (t * t) option =
       try
-        let (fst, snd) = Z.ediv_rem x.value y.value in (* Z.ediv_rem raises Divison_by_zero if y = 0*)
-        Some ({ value = fst }, { value = snd })
+        let (fst, snd) = Z.ediv_rem x (Mutez.of_Zt y) in (* Z.ediv_rem raises Divison_by_zero if y = 0*)
+        Some (fst, snd)
       with Division_by_zero -> None
 
-    let value x = x.value (* 'pretty printer' for now *)
+    let compare (x : t) (y : t) : int = Z.compare (to_Zt x) (to_Zt y)
+
     (*let format fmt_elt fmt x = (fun x -> Format.fprintf fmt "%a" x)*)
 
 end;;
-
