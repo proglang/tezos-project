aab39a482ea4ce8860069d76675f5d3a46d2ada9
Joshua <joshua@kosmosvogel.de>
2021-11-04 15:10:00 +0100

    refactored tstamp to accept input string in seconds

--- projects/ocaml_michelson-interpreter/src/lib/utils/tstamp.ml
+++ projects/ocaml_michelson-interpreter/src/lib/utils/tstamp.ml
@@ -5,9 +5,9 @@
   or as a number of seconds since Epoch in a natural (e.g. 1571659294)
 *)
 
-let of_rfc3339 (time : string) : Z.t =
+let of_rfc3339 (t : string) : Z.t =
   (*
-  Converts strings in RFC 3339 Format to Z.t in seconds since Epoch.
+  Converts timestamp-strings, that are either in RFC-3339-Format or in seconds, to Z.t (in seconds since Epoch).
 
   TODO: Z.of_float truncates the milliseconds derived from the RFC 3339 format.
   It is not known to me if it is instead rounded in the Michelson implementation.
@@ -15,13 +15,16 @@ let of_rfc3339 (time : string) : Z.t =
   two timestamps, where one was deriveded from the RFC 3339 format and the
   other one was given as an integer
   *)
-  let p_t = Ptime.of_rfc3339 ~strict:true time in
-  match p_t with
-  | Ok (rfc,_,_) -> Z.of_float(Ptime.to_float_s rfc)
-  | Error _ -> failwith "Tstamp: Timestamp invalid"
+  try
+    let p_t = Ptime.of_rfc3339 ~strict:true t in
+    match p_t with
+    | Ok (rfc,_,_) -> Z.of_float (Ptime.to_float_s rfc)
+    | Error _ -> Z.of_string t (* if t is not in RFC3339 then it might be in seconds *)
+  with _ -> failwith "Tstamp: invalid timestamp" (* exception of (Z.of_string t) *)
 
-(* Conversion of Z.t x to RFC 3339 format: *)
+
+(* Conversion of Z.t to RFC-3339 format: *)
 let to_rfc3339 (x : Z.t) : string =
   match Ptime.of_float_s (Z.to_float x) with
   | None -> failwith "Tstamp: Conversion to RFC 3339 failed"
-  | Some y -> Ptime.to_rfc3339(y);
+  | Some y -> Ptime.to_rfc3339 y;
