a961d0faccd386237a6c379cc6d479e2e7ab6255
doanha86 <33692612+doanha86@users.noreply.github.com>
2022-02-17 12:00:22 +0100

    update p

--- papers/assertion-contract/code/primes/on_off_chain_assertion.sol
+++ papers/assertion-contract/code/primes/on_off_chain_assertion.sol
@@ -4,32 +4,38 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * On/off-chain assertion contract 
- * to check counterexamples/computation proofs
+ * to check counterexamples/computational proofs
  */
 contract Validator{
 
     /**
-    * The difficulty for a computation proof
+    * The difficulty for a computational proof
     */
     uint diff = 1;
     uint public target = 2 ** (256 - diff); 
 
     /**
-    * The result: 0 = unaward, 1 = proof, 2 = counterexample
+    * The result: 0 = non-award, 1 = proof, 2 = counterexample
     */
 
-    function validate(uint p, uint seed) public view returns (uint) {
-        uint range = p / 2 - 2; 
+    function validate(uint a, uint seed) 
+    public view returns (uint) {
+        uint range = a / 2 - 2; 
         uint r = seed % range + 2;
-        uint cal = p % r;
+        uint cal = a % r;
+        bool p = (cal == 0);
+        
 
-        uint result = 0; // an unaward computation proof 
-        if (cal == 0)  result = 2; // a counterexample
+        uint result = 0; // an non-award computational proof 
+        if (p)  
+            result = 2; // a counterexample
         else 
             {
-                uint result_target = uint (keccak256(abi.encodePacked(seed, cal)));
-                if (result_target <= target) result = 1; // a computation proof    
-            }                     
+                uint result_target = 
+                    uint (keccak256(abi.encodePacked(seed, cal)));
+                if (result_target <= target) 
+                    result = 1; // a computational proof    
+            }
         return result;           
     }
 }
