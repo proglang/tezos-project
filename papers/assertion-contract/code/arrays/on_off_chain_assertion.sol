76fd01b71fb029f05bf536b6a4c2ed33f9c9a676
doanha86 <33692612+doanha86@users.noreply.github.com>
2022-02-17 12:01:02 +0100

    update p

--- papers/assertion-contract/code/arrays/on_off_chain_assertion.sol
+++ papers/assertion-contract/code/arrays/on_off_chain_assertion.sol
@@ -4,38 +4,44 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * On/off-chain assertion contract 
- * to check counterexamples/computation proofs
+ * to check counterexamples/computational proofs
  */
 contract Validator{
 
     /**
-    * The difficulty for a computation proof
+    * The difficulty for a computational proof
     */
     uint diff = 1;
     uint public target = 2 ** (256 - diff); 
 
     /**
-    * The result: 0 = unaward, 1 = proof, 2 = counterexample
+    * The result: 0 = non-award, 1 = proof, 2 = counterexample
     */
 
-    function validate(uint[] memory p, uint seed)
+    function validate(uint[] memory a, uint seed)
     public view returns (uint) {
-        uint range_1 = p.length - 1; 
+        uint range_1 = a.length - 1; 
         require(range_1 > 0, "invalid range");
-        uint n = seed_i % range_1 + 1;
+        uint n = seed % range_1 + 1;
         uint range_2 = n;
         require(range_2 > 0, "invalid range");
-        uint m = seed_j % range_2 ;
+        uint seed_1 = uint (keccak256(abi.encodePacked(seed, n)));
+        uint m = seed_1 % range_2 ;
         
-        uint cal_1 = p[m];
-        uint cal_2 = p[n];
+        uint cal_1 = a[m];
+        uint cal_2 = a[n];
+        
+        bool p = (cal_1 > cal_2);
 
-        uint result = 0; // an unaward computation proof 
-        if (cal_1 > cal_2)  result = 2; // a counterexample
+        uint result = 0; // an non-award computational proof 
+        if (p)  
+            result = 2; // a counterexample
         else 
             {
-            uint result_target = uint (keccak256(abi.encodePacked(seed, cal_1, cal_2)));
-            if (result_target <= target) result = 1; // a computation proof      
+            uint result_target = 
+                uint (keccak256(abi.encodePacked(seed, cal_1, cal_2)));
+            if (result_target <= target) 
+                result = 1; // a computational proof      
         }             
         return result;           
     }
